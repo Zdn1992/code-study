5f1bcc6aaac0504fbbcdec4baac22959cc29eb20
Zdn1992 <215585990@qq.com>
2019-06-12 09:26:39 +0800

    生产者-消费者模式练习

--- src/main/java/com/zdn/thread/lock/Good.java
+++ src/main/java/com/zdn/thread/lock/Good.java
@@ -11,15 +11,15 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Good {
 
-    ReentrantLock lock = new ReentrantLock();
+    private ReentrantLock lock = new ReentrantLock();
 
-    Condition condition = lock.newCondition();
+    private Condition condition = lock.newCondition();
 
-    boolean flag = false;
+    private boolean flag = false;
     /**
      * 产品个数
      */
-    int number = 1;
+    int number = 0;
 
     public void product(){
         try {
@@ -28,7 +28,11 @@ public class Good {
               // 生产者等待
               condition.await();
             }
-
+            number ++;
+            System.out.println(Thread.currentThread().getName() + "生产商品,库存: " + number);
+            Thread.sleep(500);
+            flag = true;
+            condition.signalAll();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -36,4 +40,35 @@ public class Good {
         }
     }
 
+    public void consumer(){
+        try {
+            lock.lock();
+            while (!flag){
+                condition.await();
+            }
+            number --;
+            System.out.println(Thread.currentThread().getName() + "消费商品,库存： " + number);
+            Thread.sleep(500);
+            flag = false;
+            condition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        Good good = new Good();
+        new Thread(() -> {
+            for (;;){
+                good.product();
+            }
+        },"生产者").start();
+        new Thread(() -> {
+            for (;;){
+                good.consumer();
+            }
+        },"消费者").start();
+    }
 }
